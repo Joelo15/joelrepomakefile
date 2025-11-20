af21b4b8b37f3da82ebb0cdf88a9195741716707
Joelo15 <dylanjoelmolina@outlook.com>
2025-11-20 14:34:10 -0500

    Update app.py

--- stack/app.py
+++ stack/app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hola desde Flask con Traefik 🚀</h1>"
+    return "<h1>Hola desde Flask con Traefik Pagina de joelo 🚀</h1>"
 
 @app.route('/saludo/<nombre>')
 def saludo(nombre):
-    return f"<h2>Hola {nombre}, bienvenido a pgmoreno.byronrm.com</h2>"
+    return f"<h2>Hola {nombre}, bienvenido a pgjoelo.byronrm.com</h2>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8089)
+
